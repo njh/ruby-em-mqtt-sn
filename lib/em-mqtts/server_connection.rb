c18bf98273c6af8236c77734da08eed0dc71cc7d
Nicholas J Humfrey <njh@aelius.com>
2013-03-14 23:38:48 +0000

    Added support to Gateway for responding to Register requests

--- lib/em-mqtts/server_connection.rb
+++ lib/em-mqtts/server_connection.rb
@@ -24,11 +24,13 @@ class EventMachine::MQTTS::ServerConnection < EventMachine::Connection
     case packet
       when EventMachine::MQTTS::Packet::Connect
         connect(packet)
+      when EventMachine::MQTTS::Packet::Register
+        register(packet)
       else
         logger.warn("Unable to handle packet of type: #{packet.class}")
     end
   end
-  
+
   def state
     peername = get_peername
     if @clients.has_key?(peername)
@@ -38,15 +40,28 @@ class EventMachine::MQTTS::ServerConnection < EventMachine::Connection
       @clients[peername] = EventMachine::MQTTS::ClientState.new(address, port)
     end
   end
-  
+
   def connect(packet)
     # Mark the client as connected
     state.connected = true
     state.client_id = packet.client_id
     state.keep_alive = packet.keep_alive
 
-    connack = EventMachine::MQTTS::Packet::Connack.new(:return_code => 0)
+    connack = EventMachine::MQTTS::Packet::Connack.new(
+        :return_code => 0x00
+    )
     send_packet(connack)
+  end
+
+  def register(packet)
+    state.topic_map[packet.topic_id] = packet.topic_name
+
+    regack = EventMachine::MQTTS::Packet::Regack.new(
+      :topic_id => packet.topic_id,
+      :message_id => packet.message_id,
+      :return_code => 0x00
+    )
+    send_packet(regack)
   end
 
 end
